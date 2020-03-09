1c6f533f05dbae5e7e1f56ed71296eebeb731d69
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-09 11:06:50 +0100

    job05/index.php + job05/script.js

---
+++ jour01/job05/script.js
@@ -0,0 +1,6 @@
+function afficherjourssemaines(){
+    var jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    for(var i = 0; i <= 6; i++){
+        document.write(jourssemaines[i]);
+    }
+}
