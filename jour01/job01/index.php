7997d699a04e5f3f2f36875999cc45e7fea765b8
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-09 10:29:52 +0100

    job01/index.php

---
+++ jour01/job01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script>
+            window.alert('Hello Javascript!','menubar=no, scrollbars=no, top=100, left=100, width=300, height=200');
+        </script>
+    </head>
+    <body>
+        
+    </body>
+</html>
