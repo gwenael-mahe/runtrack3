21d879058fdeed228e8e5ed8d195a48512dc7fa9
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-10 09:50:43 +0100

    job01/index.php + job01/script.js

---
+++ jour02/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <script type="text/javascript" src="script.js"></script>
+    </head>
+    <body>
+        <section>
+            <article id="citation">
+                <p>La vie a beaucoup plus d’imagination que nous</p>
+            </article>
+            <button class="button" onclick="citation();"></button>
+        </section>
+    </body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,5 @@
+function citation(){
+    var recup = document.getElementById('citation');
+    var text = recup.innerText;
+    console.log(text);
+}
