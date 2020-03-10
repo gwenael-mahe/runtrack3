5f6b81fccc304c004092bb064d9e28b7c89e3c33
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-10 11:43:13 +0100

    job03/index.php + job03/script.js

---
+++ jour02/job03/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <button id="button">Add</button>
+        <article id="compteur">
+            0
+        </article>
+    </body>
+    <script type="text/javascript" src="script.js"></script>
+</html>
---
+++ jour02/job03/script.js
@@ -0,0 +1,5 @@
+function addone(){
+    document.getElementById('compteur').innerHTML++;
+}
+var button = document.getElementById('button');
+    button.onclick = addone;
