30280ebeb7bcf1b34819c33228fff5a204638374
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-10 16:05:42 +0100

    job06/index.php + job06/script.js + job06/style.css

---
+++ jour02/job06/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script type="text/javascript" src="script.js"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body id="body">
+        
+    </body>
+</html>
