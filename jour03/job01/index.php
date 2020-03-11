d1e32f04617386e881ac06806d9d4daf07fd0c77
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-11 09:46:29 +0100

    job01/index.php + job01/script.js

---
+++ jour03/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+    </head>
+    <body>
+        <button id="button">Show</button>
+        <p id="texte">le $ est un bon élémentmonétaire.
+        Le # de twitter est une bonne I D.
+        Il faudra faire le point avec la classe pour cacher cet élément.</p>
+        <button id="hidebutton">Hide</button>
+    </body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){     
+    $("#hidebutton").click(function(){
+        $("#texte").hide();
+    });
+    
+    $("#button").click(function(){
+        $("#texte").show();
+    });
+});
