7f919b382d00b26a00c59ea418b51bed1aa32137
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-03-12 16:11:36 +0100

    modification du return

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,5 +1,5 @@
 function jsonValueKey(ficher,key){
     var text = JSON.parse(ficher);
-    console.log(text[key]);
+    return text[key];
 }
 jsonValueKey('{"name": "La Plateforme_", "address": "8 rue d\'hozier", "city": "Marseille", "nb_staff": "11", "creation":"2019"}', "city");
